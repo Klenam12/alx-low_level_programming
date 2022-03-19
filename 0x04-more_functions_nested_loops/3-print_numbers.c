1831881dc7ecc4312b657450c731440d07522c03
Klenam12 <klenamtsagli@gmial.com>
2022-03-19 13:31:39 -0700

    hello

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,13 @@
 void print_numbers(void)
 {
 	int i;
+
 	i = 0;
 
 	while (i < 10)
-{
-	_putchar (i + '0');
-	i++;
+	{
+		_putchar (i + '0');
+		i++;
+	}
+	_putchar ('\n');
 }
-       _putchar ('\n');
-       }
